9827b57cdf65976cf13f9fe194e231a055d86855
袁恩志 <710575900@qq.com>
2019-12-16 00:13:06 +0800

    app-project

--- mvc-modules/mvc/src/main/java/org/yuan/boot/mvc/controller/ResultController.java
+++ mvc-modules/mvc/src/main/java/org/yuan/boot/mvc/controller/ResultController.java
@@ -2,8 +2,10 @@ package org.yuan.boot.mvc.controller;
 
 import org.yuan.boot.mvc.pojo.Result;
 
+
 public class ResultController {
-    public Result result(int code, String message) {
-        return new Result(code, message);
+
+    public Result<Object> result(int code, String message) {
+        return new Result<>(code, message);
     }
 }
