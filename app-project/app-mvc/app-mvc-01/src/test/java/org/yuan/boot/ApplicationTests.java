60eb15f7532b66de990bd111d9a23480d4d1800d
yuanenzhi <710575900@qq.com>
2019-12-16 12:45:21 +0800

    修改版本为0.0.2-DEV

---
+++ app-project/app-mvc/app-mvc-01/src/test/java/org/yuan/boot/ApplicationTests.java
@@ -0,0 +1,16 @@
+package org.yuan.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @program: learning-demo-01
+ * @description:
+ * @author: yuanez
+ * @create: 2019-12-16 12:38
+ **/
+@SpringBootTest(classes = Application.class)
+public class ApplicationTests {
+
+}
