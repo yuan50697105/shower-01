3c1aa22fb047b5f9a6c2e8f3aa0a2ce4c773079f
yuanenzhi <710575900@qq.com>
2019-12-19 11:09:30 +0800

    source doc

--- app-project/app-mvc/app-mvc-jdbc-01/src/main/java/org/yuan/boot/app/service/impl/OrderInfoServiceImpl.java
+++ app-project/app-mvc/app-mvc-jdbc-01/src/main/java/org/yuan/boot/app/service/impl/OrderInfoServiceImpl.java
@@ -7,12 +7,6 @@ import org.yuan.boot.app.repository.OrderInfoRepository;
 import org.yuan.boot.app.service.OrderInfoService;
 import org.yuan.boot.db.jdbc.service.impl.BaseServiceImpl;
 
-/**
- * @program: learning-demo-01
- * @description:
- * @author: yuanez
- * @create: 2019-12-17 12:34
- **/
 @Service
 public class OrderInfoServiceImpl extends BaseServiceImpl<OrderInfo, OrderInfoRepository, OrderInfoDao> implements OrderInfoService {
 }
