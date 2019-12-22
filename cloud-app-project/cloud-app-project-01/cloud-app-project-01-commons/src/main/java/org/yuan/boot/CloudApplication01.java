4edf88132b55924b47195b9c4d192e70de19abb2
袁恩志 <710575900@qq.com>
2019-12-22 21:53:37 +0800

    WebMvcRxApplication WebFluxApplication CloudAppModules

---
+++ cloud-app-project/cloud-app-project-01/cloud-app-project-01-commons/src/main/java/org/yuan/boot/CloudApplication01.java
@@ -0,0 +1,12 @@
+package org.yuan.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudApplication01 {
+    public static void main(String[] args) {
+        SpringApplication.run(CloudApplication01.class, args);
+    }
+
+}
