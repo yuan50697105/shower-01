d6504bb0d383ffae1823d5aea2d4b6a62e87d448
袁恩志 <710575900@qq.com>
2019-12-22 18:04:56 +0800

    合并dev01

--- webmvc-app-project/webmvc-app-project-03/src/main/java/org/yuan/boot/WebMvcApplication03.java
+++ webmvc-app-project/webmvc-app-project-03/src/main/java/org/yuan/boot/WebMvcApplication03.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WebMvcApplication02 {
+public class WebMvcApplication03 {
     public static void main(String[] args) {
-        SpringApplication.run(WebMvcApplication02.class, args);
+        SpringApplication.run(WebMvcApplication03.class, args);
     }
 }
