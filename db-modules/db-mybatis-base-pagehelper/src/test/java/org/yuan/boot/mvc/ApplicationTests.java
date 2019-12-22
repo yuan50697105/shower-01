22ade4cf70642d6358efa5e97e377fee03806db4
袁恩志 <710575900@qq.com>
2019-12-22 22:19:45 +0800

    WebMvcRxApplication WebFluxApplication CloudAppModules

--- db-modules/db-mybatis-base-pagehelper/src/test/java/org/yuan/boot/mvc/ApplicationTests.java
+++
@@ -1,9 +0,0 @@
-package org.yuan.boot.mvc;
-
-import org.springframework.boot.test.context.SpringBootTest;
-import org.yuan.boot.Application;
-
-@SpringBootTest(classes = Application.class)
-public class ApplicationTests {
-
-}
