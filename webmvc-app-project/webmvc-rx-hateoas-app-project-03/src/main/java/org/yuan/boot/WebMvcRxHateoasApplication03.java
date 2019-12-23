5c88318d63ece94e92ee6b99d3881223d49a5409
yuanenzhi <710575900@qq.com>
2019-12-23 12:09:03 +0800

    WebMvcRxHateoasApplication

---
+++ webmvc-app-project/webmvc-rx-hateoas-app-project-03/src/main/java/org/yuan/boot/WebMvcRxHateoasApplication03.java
@@ -0,0 +1,11 @@
+package org.yuan.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebMvcRxHateoasApplication03 {
+    public static void main(String[] args) {
+        SpringApplication.run(WebMvcRxHateoasApplication03.class, args);
+    }
+}
