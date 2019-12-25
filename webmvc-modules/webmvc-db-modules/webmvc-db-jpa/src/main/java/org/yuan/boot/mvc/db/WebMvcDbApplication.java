990a66a65945baa0bf57f0302970d5ce4aa22fc0
yuanenzhi <710575900@qq.com>
2019-12-25 12:08:12 +0800

    WebMvcRxHateoasApplication

--- webmvc-modules/webmvc-db-modules/webmvc-db-jpa/src/main/java/org/yuan/boot/mvc/db/WebMvcDbApplication.java
+++ webmvc-modules/webmvc-db-modules/webmvc-db-jpa/src/main/java/org/yuan/boot/mvc/db/WebMvcDbApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class WebMvcDbApplication {
     public static void main(String[] args) {
         SpringApplication.run(org.yuan.boot.db.Application.class, args);
     }
