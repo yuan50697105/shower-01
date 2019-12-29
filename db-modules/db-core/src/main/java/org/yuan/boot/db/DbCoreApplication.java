bce6fa1317fe6b93d1746b4021c134666b442af7
袁恩志 <710575900@qq.com>
2019-12-29 13:08:31 +0800

    feat: DbCoreApplication

---
+++ db-modules/db-core/src/main/java/org/yuan/boot/db/DbCoreApplication.java
@@ -0,0 +1,17 @@
+package org.yuan.boot.db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @program: learning-demo-01
+ * @description:
+ * @author: yuane
+ * @create: 2019-12-29 13:07
+ */
+@SpringBootApplication
+public class DbCoreApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DbCoreApplication.class, args);
+    }
+}
