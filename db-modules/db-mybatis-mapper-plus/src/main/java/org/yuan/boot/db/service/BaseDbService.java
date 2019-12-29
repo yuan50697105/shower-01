f33b01597176d2a624810e1a9027851e7e7b65c9
袁恩志 <710575900@qq.com>
2019-12-29 13:53:04 +0800

    feat: DbMybatisMapperPlusApplication

--- db-modules/db-mybatis-mapper-plus/src/main/java/org/yuan/boot/db/service/BaseDbService.java
+++ db-modules/db-mybatis-mapper-plus/src/main/java/org/yuan/boot/db/service/BaseDbService.java
@@ -1,9 +1,11 @@
 package org.yuan.boot.db.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.github.pagehelper.PageInfo;
 import org.yuan.boot.db.pojo.BaseEntity;
 
 import java.io.Serializable;
+import java.util.List;
 
 /**
  * @program: learning-demo-01
@@ -12,4 +14,23 @@ import java.io.Serializable;
  * @create: 2019-12-29 13:19
  */
 public interface BaseDbService<T extends BaseEntity<T>, ID extends Serializable> extends IService<T> {
+    int insert(T t);
+
+    int insertSelective(T t);
+
+    int update(T t);
+
+    int updateSelective(T t);
+
+    int delete(ID id);
+
+    int delete(Iterable<ID> ids);
+
+    T selectById(ID id);
+
+    T selectOne(T t);
+
+    List<T> selectList(T t);
+
+    PageInfo<T> selectList(T t, int page, int size);
 }
