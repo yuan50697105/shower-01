eff14ea04d7ea7caa03199fcb0c4eeba143489df
袁恩志 <710575900@qq.com>
2020-01-01 21:56:42 +0800

    ...

---
+++ buildCopyJarToDesktop.bat
@@ -0,0 +1,10 @@
+REM echo off
+call mvnw clean package 
+call mkdir %USERPROFILE%\desktop\maven 
+call mkdir %USERPROFILE%\desktop\maven\exe 
+call mkdir %USERPROFILE%\desktop\maven\lib 
+call mkdir %USERPROFILE%\desktop\maven\all 
+for /r . %%i in (*exec.jar) do call xcopy /q /a %%i %USERPROFILE%\desktop\maven\exe /y 
+for /r . %%i in (*dev.jar) do  call xcopy /q /a %%i %USERPROFILE%\desktop\maven\lib /y 
+for /r . %%i in (*dev*.jar) do call xcopy /q /a %%i %USERPROFILE%\desktop\maven\all /y 
+call mvnw clean
